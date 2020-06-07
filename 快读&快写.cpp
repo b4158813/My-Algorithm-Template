4a4a426d2862a605a4a23190dab40b83b7b86490
b4158813 <814295903@qq.com>
2020-06-07 18:30:37 +0800

    update

--- 快读&快写.cpp
+++ 快读&快写.cpp
@@ -21,7 +21,8 @@ template<class T> void read(T &x) {
 }
 
 //fast write
-inline void write(ll x){
+template<class T>
+inline void write(T x){
 	if(x<0)putchar('-'),x=-x;
     if(x>9)write(x/10);
     putchar(x%10+'0');
