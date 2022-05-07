3c9a3e71ed3152775abc1c27e675f202206ee644
b4158813 <814295903@qq.com>
2022-05-07 23:30:40 +0800

    update

--- Lucas定理.cpp
+++
@@ -1,7 +0,0 @@
-// Lucas定理求组合数，预处理阶乘fac[]
-inline ll C(ll n,ll m,ll Mod){
-	return (n<m)?0:fac[n]*qpow(fac[m],Mod-2,Mod)%Mod*qpow(fac[n-m],Mod-2,Mod)%Mod;
-}
-inline ll Lucas(ll n,ll m,ll Mod){
-	return (!m)?1:Lucas(n/Mod,m/Mod,Mod)%Mod*C(n%Mod,m%Mod,Mod)%Mod;
-}
--- exgcd.cpp
+++
@@ -1,6 +0,0 @@
-ll exgcd(ll a,ll b,ll &x,ll &y){//返回gcd
-	if(b==0){x=1;y=0;return a;}
-	ll res=exgcd(b,a%b,x,y);
-	ll tx=x;x=y;y=tx-a/b*y;
-	return res;
-}
