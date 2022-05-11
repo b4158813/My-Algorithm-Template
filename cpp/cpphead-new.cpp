e0b2e634b6998b4be19b27f180144e2050eb90b3
b4158813 <814295903@qq.com>
2022-05-11 17:15:10 +0800

    add

--- cpp/cpphead-new.cpp
+++ cpp/cpphead-new.cpp
@@ -1,18 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
-using ll = long long;
-using ull = unsigned long long;
+using ll = int64_t;
 using pii = pair<int,int>;
 
 void solve(){
     
-    for(int i=0,j=0;i<n;++i){
-        while(j<i && some quality...){
-            ...
-            j++;
-        }
-        ...
-    }
+    
     
 }
 
