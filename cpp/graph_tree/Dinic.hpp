2b1cc66bf1222ce7b3f273dd948e4f1d221839b5
b4158813 <b4158813@163.com>
2022-09-06 12:26:26 +0800

    mcmf

--- cpp/graph_tree/Dinic.hpp
+++ cpp/graph_tree/Dinic.hpp
@@ -59,22 +59,24 @@ public:
     }
 };
 
-class Dinic_mcmf {
+class MCMF {
    public:
     // e[u] -> (v, flow, cost, rev)
     vector<vector<tuple<int, long long, long long, int>>> e;
     vector<int> cur, inq, vis;
     vector<long long> dis;
+    vector<pair<int,int>> pre; // used for EK
+    vector<long long> minflow; // used for EK
     static constexpr long long INF = LLONG_MAX;
 
-    Dinic_mcmf(int n): e(n), cur(n), inq(n), vis(n), dis(n) {}
+    MCMF(int n): e(n), cur(n), inq(n), vis(n), dis(n), pre(n), minflow(n) {}
 
     void addedge(int u, int v, long long w, long long c) {
         e[u].emplace_back(make_tuple(v, w, c, (int)e[v].size()));
         e[v].emplace_back(make_tuple(u, 0, -c, (int)e[u].size() - 1));
     }
 
-    bool spfa(int s,int t){
+    bool spfa_dinic(int s,int t){
         fill(cur.begin(), cur.end(), 0);  // 当前弧
         fill(dis.begin(), dis.end(), INF);
         dis[s] = 0;
@@ -88,14 +90,14 @@ class Dinic_mcmf {
             for (auto&& [v, w, c, r] : e[u]) {
                 if (w > 0 && dis[v] > dis[u] + c) {
                     dis[v] = dis[u] + c;
-                    if(!inq[v]) q.push(v);
+                    if(!inq[v]) inq[v]=1, q.push(v);
                 }
             }
         }
         return dis[t] != INF;
     }
 
-    long long dfs(int u, int t, long long flow=INF) {
+    long long dfs_dinic(int u, int t, long long flow=INF) {
         if (u == t)
             return flow;
         long long left = flow;
@@ -104,7 +106,7 @@ class Dinic_mcmf {
             auto& [v, w, c, r] = e[u][i];
             auto& [vv, ww, cc, rr] = e[v][r];
             if (w > 0 && !vis[v] && dis[v] == dis[u] + c) {
-                long long out = dfs(v, t, min(left, w));
+                long long out = dfs_dinic(v, t, min(left, w));
                 w -= out;
                 ww += out;
                 left -= out;
@@ -116,12 +118,54 @@ class Dinic_mcmf {
         return flow - left;
     }
 
-    pair<long long, long long> mcmf(int s,int t){
+    pair<long long, long long> mcmf_dinic(int s,int t){
         long long mf = 0, mincost_mf = 0, flow = 0;
-        while(spfa(s, t)){
-            flow = dfs(s, t);
+        while(spfa_dinic(s, t)){
+            flow = dfs_dinic(s, t);
             mf += flow;
             mincost_mf += flow * dis[t];
+        }
+        return pair<long long, long long>(mf, mincost_mf);
+    }
+
+    bool spfa_EK(int s,int t){
+        queue<int> q;
+        q.push(s);
+        fill(dis.begin(), dis.end(), INF);
+        fill(minflow.begin(), minflow.end(), INF);
+        fill(pre.begin(), pre.end(), pair(-1,-1));
+        dis[s] = 0, inq[s] = 1;
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
+            inq[u] = 0;
+            for (int i=0;i<(int)e[u].size();++i) {
+                auto &&[v, w, c, r] = e[u][i];
+                if (w > 0 && dis[v] > dis[u] + c) {
+                    dis[v] = dis[u] + c;
+                    minflow[v] = min(minflow[v], w);
+                    pre[v] = pair(u, i);
+                    if(!inq[v]) inq[v]=1, q.push(v);
+                }
+            }
+        }
+        return dis[t] != INF;
+    }
+
+    pair<long long, long long> mcmf_EK(int s,int t){
+        long long mf = 0, mincost_mf = 0;
+        while(spfa_EK(s, t)){
+            mf += minflow[t];
+            mincost_mf += minflow[t] * dis[t];
+            int u = t;
+            while(u != s){
+                auto &[pv, i] = pre[u];
+                auto &[v, w, c, r] = e[pv][i];
+                auto &[vv, ww, cc, rr] = e[v][r];
+                w -= minflow[t];
+                ww += minflow[t];
+                u = pv;
+            }
         }
         return pair<long long, long long>(mf, mincost_mf);
     }
