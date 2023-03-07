991d9020084b33f8078ee1f5d94d236d1a1dfa39
b4158813 <814295902@qq.com>
2023-03-08 00:31:04 +0800

    cpphead-new

--- cpp/cpphead-new.cpp
+++ cpp/cpphead-new.cpp
@@ -5,16 +5,17 @@ using ull = unsigned long long;
 using pii = pair<int,int>;
 using tiii = tuple<int,int,int>;
 
-string to_string(const string &s){return '"' + s + '"';}
-string to_string(const char* s){return to_string((string) s);}
-string to_string(bool b){return (b ? "true" : "false");}
-template <typename A, typename B> string to_string(const pair<A, B> &p){return "(" + to_string(p.first) + "," + to_string(p.second) + ")";}
-template <typename A, typename B, typename C> string to_string(const tuple<A, B, C> &p){return "(" + to_string(get<0>(p)) + "," + to_string(get<1>(p)) + "," + to_string(get<2>(p)) + ")";}
-template <typename A, typename B, typename C, typename D> string to_string(const tuple<A, B, C, D> &p){return "(" + to_string(get<0>(p)) + "," + to_string(get<1>(p)) + "," + to_string(get<2>(p)) + "," + to_string(get<3>(p)) + ")";}
-template <typename A> string to_string(const A &v){bool first = true;string res = "[";for (const auto &x : v) {if (!first) res += ",";first = false;res += to_string(x);}res += "]";return res;}
-void debug_out(){std::cerr << endl;}
-template<class T, class... Ts> void debug_out(const T &x, const Ts &...xs){std::cerr << to_string(x) << ' '; debug_out(xs...);}
-#define debug(...) std::cerr << "[" << #__VA_ARGS__ << "]: ", debug_out(__VA_ARGS__)
+namespace std {
+    string to_string(const string& s){return '"'+s+'"';}
+    string to_string(const char* s){return to_string((string)s);}
+    string to_string(bool b){return (b?"true":"false");}
+    template <typename A> string to_string(const A& v) {bool flag{true};string s{"["};for(const auto& x:v){s+=","[flag]+to_string(x);flag=(flag?0:0);}s.append("]");return s;}
+    template <typename T, typename U> string to_string(const pair<T, U>& pa){return "("+to_string(pa.first)+","+to_string(pa.second)+")";}
+    template <class... T> string to_string(const tuple<T...>& tu){constexpr int N = tuple_size_v<tuple<T...>>;return [&]<size_t... I>(std::index_sequence<I...>){string s;return "("+(...,(s.append(to_string(get<I>(tu))+","[I==N-1])))+")";}(std::make_index_sequence<N>());}
+    template <class... T> void debug_out(const T&... args){(..., (cerr << to_string(args) << ' '));endl(cerr);}
+    #define debug(...) std::cerr << "[" << #__VA_ARGS__ << "]: ", debug_out(__VA_ARGS__)
+}
+
 
 void solve(){
     
