58b172a38b3c5972948f332662f2f36700ad2905
b4158813 <814295902@qq.com>
2023-03-10 23:24:54 +0800

    cpppheadnew

--- cpp/cpphead-new.cpp
+++ cpp/cpphead-new.cpp
@@ -11,7 +11,7 @@ namespace std {
     string to_string(bool b){return (b?"true":"false");}
     template <typename A> string to_string(const A& v) {bool flag{true};string s{"["};for(const auto& x:v){s+=","[flag]+to_string(x);flag=(flag?0:0);}s.append("]");return s;}
     template <typename T, typename U> string to_string(const pair<T, U>& pa){return "("+to_string(pa.first)+","+to_string(pa.second)+")";}
-    template <class... T> string to_string(const tuple<T...>& tu){constexpr int N = tuple_size_v<tuple<T...>>;return [&]<size_t... I>(std::index_sequence<I...>){string s;return "("+(...,(s.append(to_string(get<I>(tu))+","[I==N-1])))+")";}(std::make_index_sequence<N>());}
+    template <class... T> string to_string(const tuple<T...>& tu){constexpr size_t N = tuple_size_v<tuple<T...>>;return [&]<size_t... I>(std::index_sequence<I...>){string s;return "("+(...,(s.append(to_string(get<I>(tu))+","[I==N-1])))+")";}(std::make_index_sequence<N>());}
     template <class... T> void debug_out(const T&... args){(..., (cerr << to_string(args) << ' '));endl(cerr);}
     #define debug(...) std::cerr << "[" << #__VA_ARGS__ << "]: ", debug_out(__VA_ARGS__)
 }
@@ -27,7 +27,7 @@ int main(){
     
     std::cin.tie(nullptr)->sync_with_stdio(false);
     
-    int _ = 1;
+    int _{1};
     std::cin>>_;
     while(_--){
         
