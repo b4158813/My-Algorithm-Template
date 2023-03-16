5685ad597ea64cc1fc26d52ca6f2461f4387ad55
b4158813 <814295902@qq.com>
2023-03-16 12:07:41 +0800

    tarjan

---
+++ cpp/graph_tree/Tarjan.hpp
@@ -0,0 +1,95 @@
+/*
+        Tarjan SCC
+        有向图 强连通分量
+		
+		usage:
+		```
+		for(int i=0;i<n;++i) {
+			if(!dfn[i]) {
+				tarjan(i);
+			}
+		}
+		```
+*/
+vector<int> low(n), dfn(n), sta;
+vector<bool> vis(n);
+vector<int> color(n);
+int deep{0}, scc{0};
+function<void(int)> tarjan = [&](int u) {
+    vis[u] = 1;
+    sta.emplace_back(u);
+    low[u] = dfn[u] = ++deep;
+    for (auto &&v: e[u]) {
+        if (!dfn[v]) {
+            tarjan(v);
+            low[u] = min(low[u], low[v]);
+        } else if (vis[v]) {
+            low[u] = min(low[u], dfn[v]);
+        }
+    }
+    if (low[u] == dfn[u]) {
+        vis[u] = 0;
+        color[u] = scc;
+        while (sta.back() != u) {
+            color[sta.back()] = scc;
+            vis[sta.back()] = 0;
+            sta.pop_back();
+        }
+        sta.pop_back();
+        scc++;
+    }
+};
+
+
+/*
+    Tarjan V-DCC
+    无向图 点双连通分量 + 割点
+		
+	usage:
+	```
+	for(int i=0;i<n;++i) {
+		if(!dfn[i]) {
+			RT = i;
+			tarjan(i, i);
+		}
+	}
+	```
+*/
+vector<int> low(n), dfn(n), sta;
+vector<bool> cut(n);
+vector<vector<int>> dcc;
+int deep{0}, RT{0};
+function<void(int, int)> tarjan = [&](int u, int fa) {
+    dfn[u] = low[u] = ++deep;
+    sta.emplace_back(u);
+    if (e[u].empty() && u == fa) {
+        dcc.emplace_back(vector<int>{u});
+        return;
+    }
+    int child = 0;
+    for (auto v : e[u]) {
+        if (v == fa)
+            continue;
+        if (!dfn[v]) {
+            tarjan(v, u);
+            low[u] = min(low[u], low[v]);
+            if (low[v] >= dfn[u]) {
+                child++;
+                if (u != RT || child > 1) {
+                    cut[u] = 1;
+				}
+                dcc.emplace_back(vector<int>());
+                int t;
+                do {
+                    t = sta.back();
+                    sta.pop_back();
+                    dcc.back().emplace_back(t);
+                } while (t != v);
+                dcc.back().emplace_back(u);
+            }
+        }else {
+            low[u] = min(low[u], dfn[v]);
+		}
+    }
+    return;
+};
---
+++ cpp/graph_tree/二分图.hpp
@@ -0,0 +1,40 @@
+
+// 匈牙利算法
+int idx = 0;
+vector<int> have(n + 1), used(n + 1);
+function<bool(int)> Hungary = [&](int u) {
+    for (auto&& v : e[u]) {
+        if (used[v] != idx) {
+            used[v] = idx;
+            if (!have[v] || Hungary(have[v])) {
+                have[v] = u;
+                return true;
+            }
+        }
+    }
+    return false;
+};
+/*
+usage:
+
+    for(int i=1;i<=n;++i) {
+        idx ++;
+        if(Hungary(i)) {
+            cnt ++;
+        }
+    }
+
+*/
+
+// 二分图判定
+vector<int> colr(n + 1);
+function<bool(int, int)> isbg = [&](int u, int col) {
+    colr[u] = col;
+    for (auto&& v : e[u]) {
+        if (colr[u] == colr[v])
+            return false;
+        else if (!colr[v] && !isbg(v, 3 - col))
+            return false;
+    }
+    return true;
+};
